ca475ba2f5fcdeefb68db7616d671826998fe243
Yash Sharma <yashsharma1859@gmail.com>
2020-08-18 16:47:17 +0530

    Updates

---
+++ precache-manifest.f501963f8c37e333c3df9605791af6b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e033bdda622c1f2aeadfade3459c86e8",
+    "url": "/%7Brouter-test%7D/index.html"
+  },
+  {
+    "revision": "4eb88d487f228c7aced1",
+    "url": "/%7Brouter-test%7D/static/css/main.42c9ded9.chunk.css"
+  },
+  {
+    "revision": "4b3b0ecc57e93a50b479",
+    "url": "/%7Brouter-test%7D/static/js/2.480e35f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%7Brouter-test%7D/static/js/2.480e35f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb88d487f228c7aced1",
+    "url": "/%7Brouter-test%7D/static/js/main.a13b7da5.chunk.js"
+  },
+  {
+    "revision": "a654b83bd97ca77727a9",
+    "url": "/%7Brouter-test%7D/static/js/runtime-main.72dee88d.js"
+  }
+]);
